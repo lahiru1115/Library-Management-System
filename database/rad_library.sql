9ab2d8d72ede44621ecf9e8c9ebd5f801a69a477
lahiru1115 <lahirudissanayake15@gmail.com>
2022-09-15 14:19:29 +0530

    Create database

--- database/rad_library.sql
+++ database/rad_library.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `rad_lms`
+-- Database: `rad_library`
 --
 
 -- --------------------------------------------------------
@@ -27,6 +27,9 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `admin`
 --
+
+DROP DATABASE IF EXISTS rad_library;
+CREATE DATABASE rad_library;
 
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
