256ecafb9a176dc880767b6514692bb1e06cc457
lahiru1115 <lahirudissanayake15@gmail.com>
2022-09-16 08:06:02 +0530

    Db up

--- database/rad_library.sql
+++ database/rad_library.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 06, 2020 at 03:30 PM
--- Server version: 5.7.26
--- PHP Version: 7.2.18
+-- Generation Time: Sep 16, 2022 at 02:35 AM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -28,24 +27,22 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-DROP DATABASE IF EXISTS rad_library;
-CREATE DATABASE rad_library;
-
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
-  `sno` int(11) NOT NULL,
+  `id` int(10) NOT NULL,
   `name` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`sno`)
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`sno`, `name`, `username`, `password`) VALUES
-(1, 'sandul', 'sandul', '1234');
+INSERT INTO `admin` (`id`, `name`, `username`, `password`) VALUES
+(1, 'Admin', 'Admin', 'Admin'),
+(2, 'Admin2', '', '');
 
 -- --------------------------------------------------------
 
@@ -78,7 +75,37 @@ INSERT INTO `all_books` (`bookno`, `bookname`, `author`, `pages`, `price`) VALUE
 (10, 'java 3', 'john', 50, 500),
 (20, 'java 4', 'james', 540, 5000),
 (50, 'c# 5', 'nihal perera', 400, 500),
-(100, 'java book 1', 'kaamal', 525, 1000);
+(100, 'java book 1', 'kaamal', 525, 1000),
+(56, 'dfgd', 'dffg', 56, 200),
+(234, '2erwe', 'werwer', 34, 234);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assistant_librarian`
+--
+
+DROP TABLE IF EXISTS `assistant_librarian`;
+CREATE TABLE IF NOT EXISTS `assistant_librarian` (
+  `id` int(10) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `assistant_librarian`
+--
+
+INSERT INTO `assistant_librarian` (`id`, `fname`, `lname`, `username`, `password`) VALUES
+(1, '123', '123', 'sandul', 'sandul'),
+(55555, '123', '123', '4', '4'),
+(8979, '123', '123', '45', '67'),
+(5678678, '123', '123', '657', '567'),
+(56, '56456456', '324234terter', '34234retert', '324234rtert'),
+(43534535, '345344535', '3453453', '3453535', '3453535');
 
 -- --------------------------------------------------------
 
@@ -101,37 +128,60 @@ CREATE TABLE IF NOT EXISTS `issue_books` (
   PRIMARY KEY (`bookno`,`student_no`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `issue_books`
+-- Table structure for table `librarian`
 --
 
-INSERT INTO `issue_books` (`bookno`, `book_name`, `author`, `price`, `pages`, `student_no`, `student_name`, `date_of_issue`, `date_to_be_return`, `availability`) VALUES
-(2, 'c#', 'james', 2500, 250, 'D/BSE/19/0030', 'viraj', '05/04/2020', '25/04/2020', 'not available');
+DROP TABLE IF EXISTS `librarian`;
+CREATE TABLE IF NOT EXISTS `librarian` (
+  `id` int(10) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `librarian`
+--
+
+INSERT INTO `librarian` (`id`, `fname`, `lname`, `username`, `password`) VALUES
+(1, '123', '123', 'sandul', 'sandul'),
+(55555, '123', '123', '4', '4'),
+(5678678, '123', '123', '657', '567'),
+(234, 'df', 'df', 'df', 'df'),
+(345345, '345353', '34535', '3453453', '3453535');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `new_librarian`
+-- Table structure for table `registrar`
 --
 
-DROP TABLE IF EXISTS `new_librarian`;
-CREATE TABLE IF NOT EXISTS `new_librarian` (
-  `sno` int(11) NOT NULL,
+DROP TABLE IF EXISTS `registrar`;
+CREATE TABLE IF NOT EXISTS `registrar` (
+  `id` int(10) NOT NULL,
   `fname` varchar(255) NOT NULL,
   `lname` varchar(255) NOT NULL,
-  `mnumber` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`sno`)
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `new_librarian`
+-- Dumping data for table `registrar`
 --
 
-INSERT INTO `new_librarian` (`sno`, `fname`, `lname`, `mnumber`, `username`, `password`) VALUES
-(1, 'sandul', 'jayakody', 714276033, 'sandul', 'sandul'),
-(100, 'Sampath', 'Gamage', 71545454, 'sampath', 'sampath');
+INSERT INTO `registrar` (`id`, `fname`, `lname`, `username`, `password`) VALUES
+(1, '123', '123', 'sandul', 'sandul'),
+(55555, '123', '123', '4', '4'),
+(8979, '123', '123', '45', '67'),
+(5678678, '123', '123', '657', '567'),
+(43534535, '345344535', '3453453', '3453535', '3453535'),
+(67567, '567567', '567567', '567567', '567567');
 
 -- --------------------------------------------------------
 
@@ -161,7 +211,8 @@ INSERT INTO `return_books` (`bookno`, `book_name`, `author`, `price`, `pages`, `
 (100, 'java book 1', 'kaamal', 1000, 525, 'D/BSE/19/0025', 'sandun', '05/04/2020', '10/04/2020'),
 (50, 'c# 5', 'nihal perera', 500, 400, 'D/BSE/19/0025', 'tharaka', '05/04/2020', '20/04/2020'),
 (7, 'java 3', 'John', 500, 50, 'D/BSE/19/0020', 'sampath', '20/20/2020', '20/03/2020'),
-(20, 'java 4', 'james', 5000, 540, 'D/BSE/19/0020', 'Sameera', '06/04/2020', '12/04/2020');
+(20, 'java 4', 'james', 5000, 540, 'D/BSE/19/0020', 'Sameera', '06/04/2020', '12/04/2020'),
+(2, 'c#', 'james', 2500, 250, 'D/BSE/19/0030', 'viraj', '05/04/2020', '25/04/2020');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
